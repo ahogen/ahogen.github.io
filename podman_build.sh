a140f527c9abaf9e0ad18667b53b6def8bf162dc
Alex Hogen <code.ahogen@outlook.com>
2023-11-16 23:15:26 -0800

    ignore creds. Clone from local gitea works.

---
+++ podman_build.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/bash
+
+# # If ssh-agent not started, start it
+# if ps -p $SSH_AGENT_PID > /dev/null
+# then
+#    echo "ssh-agent is already running"
+# else
+#     echo "Starting ssh-agent"
+#     eval $(ssh-agent -s)
+# fi
+
+# --mount=type=bind,source=${SSH_AUTH_SOCK},destination=/ssh-agent \
+
+# Build with Podman
+podman run -v $PWD:/antora:Z \
+    --env NODE_TLS_REJECT_UNAUTHORIZED=0 \
+    --rm -it \
+    docker.io/antora/antora --git-credentials-path=./.git-credentials  antora-playbook.yml
